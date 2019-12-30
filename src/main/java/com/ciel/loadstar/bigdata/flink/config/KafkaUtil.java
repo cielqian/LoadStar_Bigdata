c7a64828b919d0ff332797bf8e1ced278f4d4a5b
CielQian <qianhong91@outlook.com>
2019-12-31 05:30:07 +0800

    修改consumer

--- src/main/java/com/ciel/loadstar/bigdata/flink/config/KafkaUtil.java
+++ src/main/java/com/ciel/loadstar/bigdata/flink/config/KafkaUtil.java
@@ -6,7 +6,7 @@ public class KafkaUtil {
     public static Properties getProperties(){
         Properties props = new Properties();
         props.put("bootstrap.servers", "kafka.loadstar.top:9093");
-        props.put("group.id", "flink-group-master1");
+        props.put("group.id", "bigdata-flink-group-master");
         props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer"); //key 序列化
         props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer"); //value 序列化
 
